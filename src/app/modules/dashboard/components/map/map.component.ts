dde87799cf0965af21dfcd20f43aabc892a8a2e4
clickyman <baotoan.95@gmail.com>
2022-05-15 23:51:33 +0700

    Custom marker icon and add clusters

--- src/app/modules/dashboard/components/map/map.component.ts
+++ src/app/modules/dashboard/components/map/map.component.ts
@@ -12,7 +12,7 @@ import { AppConfig } from 'src/app/config/app.config';
 export class MapComponent implements OnInit {
   baseUrl = AppConfig.endpoints.baseUrl;
   centerCoord: ICoord;
-  @Input() markers = [];
+  @Input() markers: any = [];
 
   previous;
 
@@ -21,6 +21,11 @@ export class MapComponent implements OnInit {
     private spinner: Spinner,
     private locationService: LocationService
   ) { }
+
+  private getRandomInRange(from, to, fixed) {
+    return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
+    // .toFixed() returns string, so ' * 1' is a trick to convert to number
+  }
 
   ngOnInit() {
     this.centerCoord = JSON.parse(sessionStorage.getItem('position'));
@@ -37,8 +42,22 @@ export class MapComponent implements OnInit {
 
     this.spinner.show();
     this.locationService.getAdvPosition().subscribe(rs => {
-      this.markers = rs;
-      this.markers.splice(0, 3500);
+      // this.markers = rs;
+      for (let i = 0; i < 1000; i++) {
+        this.markers.push({
+          longitude: this.getRandomInRange(-180, 180, 8),
+          latitude: this.getRandomInRange(-180, 180, 8),
+          advId: 1234,
+          avatar: {
+            url: 'https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989_1280.jpg',
+            scaledSize: {
+              width: 40,
+              height: 40
+            }
+          }
+        });
+      }
+      // this.markers.splice(0, 3500);
       console.log(this.markers);
       this.spinner.hide();
     });
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -4,8 +4,9 @@ import { DashboardComponent } from './dashboard.component';
 import { MapComponent } from './components/map/map.component';
 import { DashboardRoutes } from './dashboard-routes';
 import { AgmCoreModule } from '@agm/core';
-import { GeoLocationService } from 'src/app/shared/utils/geo-location.service';
+import { GeoLocationService } from '../../shared/utils/geo-location.service';
 import { LocationService } from './shared/location.service';
+import { AgmMarkerClustererModule } from '@agm/markerclusterer';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { LocationService } from './shared/location.service';
     DashboardRoutes,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyDCa1LUe1vOczX1hO_iGYgyo8p_jYuGOPU'
-    })
+    }),
+    AgmMarkerClustererModule
   ],
   providers: [
     GeoLocationService,
